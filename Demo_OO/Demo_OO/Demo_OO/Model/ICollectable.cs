b80f491aa7d0ca25dcda05bc83b9988584f2f7bf
Lies <Lies@DESKTOP-LD27GAE>
2017-10-09 11:37:43 +0200

    Demo OO finished + pdf added

---
+++ Demo_OO/Demo_OO/Demo_OO/Model/ICollectable.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo_OO.Model
+{
+    /// <summary>
+    /// interface to determine collectable items, 
+    /// such as wine or comic books
+    /// </summary>
+    public interface ICollectable
+    {
+        int YearOfOrigin { get; set; }
+    }
+}
